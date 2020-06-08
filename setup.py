5205ac38384c66f8575eb3481b47dda76d11b922
Paige <paige.c.chang@gmail.com>
2020-06-08 11:58:20 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name = 'Personalized-Music-Recommendation',
+      author = 'Paige Chang',
+      author_email= = 'paige.c.chang@gmail.com',
+      url = 'https://github.com/paige-chang/Personalized-Music-Recommendation',
+      packages = ['Music-Recommendation']
+)
