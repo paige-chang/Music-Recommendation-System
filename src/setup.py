897fcc7c32e06a24bc564be4dca065f352d600e0
Paige <paige.c.chang@gmail.com>
2020-07-04 21:33:45 -0400

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
-setup(name = 'Personalized-Music-Recommendation',
+setup(name = 'Music-Recommendation-System',
       author = 'Paige Chang',
       author_email= = 'paige.c.chang@gmail.com',
-      url = 'https://github.com/paige-chang/Personalized-Music-Recommendation',
+      url = 'https://github.com/paige-chang/Music-Recommendation-System',
       packages = ['Music-Recommendation']
 )
